159a75269c4e889faf92e61256bed14b97ffc671
jmolly <roshkovskypetr@gmail.com>
2019-06-06 22:57:51 +0300

    Updates

---
+++ precache-manifest.8472c401ce88494df4884c25c13a43c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383209edc245b6ce22162638acc427bc",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "db0a0499a8b55b18d608",
+    "url": "/tic-tac-toe/static/css/main.d807f2cf.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "db0a0499a8b55b18d608",
+    "url": "/tic-tac-toe/static/js/main.f560d02e.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
